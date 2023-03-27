db06939d97350decd7daa14fcb02b772df1f6cdf
admin <admin@gmail.com>
2023-03-28 01:50:42 +0530

    update login page

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,19 +1,24 @@
-package com.mycompany.app;
+<form action="action_page.php" method="post">
+  <div class="imgcontainer">
+    <img src="img_avatar2.png" alt="Avatar" class="avatar">
+  </div>
 
-/**
- * Hello world!
- */
-public class App {
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
 
-    private static final String MESSAGE = "Hello World!";
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
 
-    public App() {}
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
 
-    public static void main(String[] args) {
-        System.out.println(MESSAGE);
-    }
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
 
-    public String getMessage() {
-        return MESSAGE;
-    }
-}
